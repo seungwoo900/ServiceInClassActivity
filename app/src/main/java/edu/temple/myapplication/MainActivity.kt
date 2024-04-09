b7c9f79657b5ace0e880663417deae387c588b00
Daniel Lee <tur95049@temple.edu>
2024-04-09 11:32:50 -0400

    Bind service to main activity

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,14 +1,39 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var timerBinder: TimerService.TimerBinder
+    var isConnected = false
+
+    val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isConnected = false
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
 
         findViewById<Button>(R.id.startButton).setOnClickListener {
 
@@ -22,4 +47,6 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+
 }
