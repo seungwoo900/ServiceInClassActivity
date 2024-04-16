951acb033c54dfb98983a3c14a0ae3101c1c465e
Daniel Lee <tur95049@temple.edu>
2024-04-16 11:43:14 -0400

    Override functions to use menu buttons

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -8,7 +8,11 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -62,5 +66,30 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         unbindService(serviceConnection)
         super.onDestroy()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_start_timer -> Toast.makeText(this, "Starting timer", Toast.LENGTH_SHORT).show()
+
+            R.id.action_pause_timer -> Toast.makeText(this, "Pausing timer", Toast.LENGTH_SHORT).show()
+
+            R.id.action_stop_timer -> {
+                AlertDialog.Builder(this)
+                    .setTitle("Stop confirmation")
+                    .setMessage("Are you sure you would like to stop this timer")
+                    .setPositiveButton("Yes"){dialog,_ -> Toast.makeText(this, "Stopping...", Toast.LENGTH_SHORT).show(); dialog.dismiss()}
+                    .setNegativeButton("Never mind"){dialog,_ -> dialog.cancel()}
+                    .show()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
